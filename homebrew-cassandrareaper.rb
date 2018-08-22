04c6da68927c17eb3bda8f7ba7def56f0937aa11
Kotaro Yamazaki <s1411420@u.tsukuba.ac.jp>
2018-08-22 17:25:54 +0900

    update url version sha256

--- homebrew-cassandrareaper.rb
+++ homebrew-cassandrareaper.rb
@@ -4,8 +4,10 @@
 class HomebrewCassandrareaper < Formula
   desc "test"
   homepage "http://cassandra-reaper.io"
-  head "https://github.com/KotaroYamazaki/homebrew-CassandraReaper.git"
-  
+  url "https://github.com/thelastpickle/cassandra-reaper/releases/download/1.2.2/cassandra-reaper-1.2.2-release.tar.gz"
+  version "1.2.2"
+  sha256 "720aff69e3205301bc07399afc46dae3568d8effffa3712f1852a169ce9801db"
+
   depends_on "bower" => :build
   depends_on "maven" => :build
 
