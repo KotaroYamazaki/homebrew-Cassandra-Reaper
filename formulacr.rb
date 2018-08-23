c675e6c29b5b0be052df79cc7cc7322a711bab8f
Kotaro Yamazaki <s1411420@u.tsukuba.ac.jp>
2018-08-23 14:54:19 +0900

    class name: fomulacr -> Formulacr

--- formulacr.rb
+++ formulacr.rb
@@ -1,4 +1,4 @@
-class formulacr < Formula
+class Formulacr < Formula
   homepage "http://cassandra-reaper.io"
   url "https://github.com/thelastpickle/cassandra-reaper/releases/download/1.2.2/cassandra-reaper-1.2.2-release.tar.gz"
   version "1.2.2"
