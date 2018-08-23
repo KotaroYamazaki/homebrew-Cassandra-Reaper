735f7c4453fb51d65152b9e3625aea5749ae791f
Kotaro Yamazaki <s1411420@u.tsukuba.ac.jp>
2018-08-23 13:59:27 +0900

    delete message

--- homebrew-cassandrareaper.rb
+++ homebrew-cassandrareaper.rb
@@ -12,24 +12,8 @@ class HomebrewCassandrareaper < Formula
   depends_on "maven" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # system "cmake", ".", *std_cmake_args
     Dir.chdir "src"
     system "mvn", "clean", "package" # if this fails, try separate make/make install steps
     prefix.install "server/target/cassandra-reaper-1.2.2.jar"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test homebrew-CassandraReaper`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
