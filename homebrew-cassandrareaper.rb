6967f6bfca37b4cdba4f7e346280334bf8b3c3e3
Kotaro Yamazaki <s1411420@u.tsukuba.ac.jp>
2018-08-23 14:37:39 +0900

    fix install command

--- homebrew-cassandrareaper.rb
+++ homebrew-cassandrareaper.rb
@@ -1,16 +1,13 @@
 class HomebrewCassandrareaper < Formula
-  desc "test"
   homepage "http://cassandra-reaper.io"
   url "https://github.com/thelastpickle/cassandra-reaper/releases/download/1.2.2/cassandra-reaper-1.2.2-release.tar.gz"
   version "1.2.2"
   sha256 "720aff69e3205301bc07399afc46dae3568d8effffa3712f1852a169ce9801db"
 
-  depends_on "bower" => :build
-  depends_on "maven" => :build
-
   def install
-    Dir.chdir "src"
-    system "mvn", "clean", "package" # if this fails, try separate make/make install steps
-    prefix.install "server/target/cassandra-reaper-1.2.2.jar"
+    prefix.install "bin"
+    mv "server/target" "cassandrareaper"
+    etc.install "cassandrareaper"
+    share.install "cassandrareaper"
   end
 end
