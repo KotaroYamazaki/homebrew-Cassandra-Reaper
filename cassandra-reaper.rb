6ebd9718e05a1bc94b4d57a41129222f5b092a1c
Kotaro Yamazaki <s1411420@u.tsukuba.ac.jp>
2018-08-24 11:59:05 +0900

    delete version 1.2.2. * Stable: version 1.2.2 is redundant with version scanned from URL

--- cassandra-reaper.rb
+++ cassandra-reaper.rb
@@ -1,7 +1,6 @@
 class CassandraReaper < Formula
   homepage "http://cassandra-reaper.io"
   url "https://github.com/thelastpickle/cassandra-reaper/releases/download/1.2.2/cassandra-reaper-1.2.2-release.tar.gz"
-  version "1.2.2"
   sha256 "720aff69e3205301bc07399afc46dae3568d8effffa3712f1852a169ce9801db"
 
   def install
