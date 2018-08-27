4b18cf5531952c93440ed298a09000df2f52e12c
Kotaro Yamazaki <s1411420@u.tsukuba.ac.jp>
2018-08-27 16:22:00 +0900

    add patch option :p1

--- cassandra-reaper.rb
+++ cassandra-reaper.rb
@@ -4,7 +4,7 @@ class CassandraReaper < Formula
   url "https://github.com/thelastpickle/cassandra-reaper/releases/download/1.2.2/cassandra-reaper-1.2.2-release.tar.gz"
   sha256 "720aff69e3205301bc07399afc46dae3568d8effffa3712f1852a169ce9801db"
 
-  patch :p0 do
+  patch :p1 do
     url "https://github.com/thelastpickle/cassandra-reaper/commit/4e26e1c70de8aa564e57ada287fffd6e7544914f.patch"
     sha256 "6d2bdf3f574eccb90c339b498a8cc8663a11ccdc0596af8e5c492869e77920e5"
   end
