52ff47b52ede2f279f3c9b80236825bd12cfc1a5
Kotaro Yamazaki <s1411420@u.tsukuba.ac.jp>
2018-08-27 14:48:24 +0900

    modify 'end position of fork do'

--- cassandra-reaper.rb
+++ cassandra-reaper.rb
@@ -16,9 +16,9 @@ class CassandraReaper < Formula
     begin
       pid = fork do
         exec "/usr/local/bin/cassandra-reaper"
-      end
       output = shell_output("curl -Im3 -o- http://localhost:8080/webui/")
       assert_match /200 OK.*Content/m, output
+      end
     ensure
       Process.kill("HUP", pid)
     end
